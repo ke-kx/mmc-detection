36f5634734eadadf7df661e69fb53e9bae0abee3
Nils Kunze <kunze.nils@gmail.com>
2018-02-13 23:57:13 +0100

    add detector

---
+++ dmmc/detectors/almostequal.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+
+# todo if ae too slow:
+# idea: already build call vectors (1 if call present) when generating tus
+# then it's as simple as subtracting the vectors and checking the abs (right?)
+
+
+class Score(object):
+    def __init__(self):
+        self.e = 0
+        self.ae = 0
+        self.missingcalls = defaultdict(int)
+
+    def score(self):
+        return 1.0 - (self.e / (self.e + self.ae))
+
+    def __str__(self):
+        return "Score: {0} (E: {1}, AE: {2}, missing: {3}"\
+            .format(self.score, self.e, self.ae, self.missingcalls)
+
+    def __repr__(self):
+        return "Score: {0}".format(self.score())
+
+
+class AlmostEqualDetector(object):
+    def __init__(self, n):
+        self.n = n
+
+    def analyze(self, typeusages):
+        """Go through all given TUs and check how anomalous they are.
+        Return a dict with id to Score object mapping
+        """
+        scores = defaultdict(Score)
+        typeusages = list(typeusages)
+
+        for idx, tu_a in enumerate(typeusages):
+            for tu_b in typeusages[idx:]:
+                if self._equal(tu_a, tu_b):
+                    scores[tu_a.id].e += 1
+                    scores[tu_b.id].e += 1
+                elif self._almost_equal(tu_a, tu_b):
+                    score = scores[tu_a.id]
+                    score.ae += 1
+
+                    missing = frozenset(tu_b.calls - tu_a.calls)
+                    score.missingcalls[missing] += 1
+                elif self._almost_equal(tu_b, tu_a):
+                    score = scores[tu_b.id]
+                    score.ae += 1
+
+                    missing = frozenset(tu_a.calls - tu_b.calls)
+                    score.missingcalls[missing] += 1
+
+        return scores
+
+    @staticmethod
+    def _equal(tu_a, tu_b):
+        return tu_a.calls == tu_b.calls
+
+    def _almost_equal(self, tu_a, tu_b):
+        if len(tu_a.calls) + self.n != len(tu_b.calls):
+            return False
+
+        return tu_a.calls.issubset(tu_b.calls)
